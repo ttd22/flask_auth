e93bc0bce4a220e20129e690eceb73d5641af765
trangttdang <trangttdang01@gmail.com>
2022-04-19 23:55:22 -0400

    update project

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -7,9 +7,6 @@ from app.db import db
 from app.db.models import User
 
 auth = Blueprint('auth', __name__, template_folder='templates')
-from flask import current_app
-
-
 
 
 @auth.route('/login', methods=['POST', 'GET'])
@@ -72,75 +69,6 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-
-@auth.route('/users')
-@login_required
-@admin_required
-def browse_users():
-    data = User.query.all()
-    titles = [('email', 'Email'), ('registered_on', 'Registered On')]
-    retrieve_url = ('auth.retrieve_user', [('user_id', ':id')])
-    edit_url = ('auth.edit_user', [('user_id', ':id')])
-    add_url = url_for('auth.add_user')
-    delete_url = ('auth.delete_user', [('user_id', ':id')])
-
-    current_app.logger.info("Browse page loading")
-
-    return render_template('browse.html', titles=titles, add_url=add_url, edit_url=edit_url, delete_url=delete_url,
-                           retrieve_url=retrieve_url, data=data, User=User, record_type="Users")
-
-
-@auth.route('/users/<int:user_id>')
-@login_required
-def retrieve_user(user_id):
-    user = User.query.get(user_id)
-    return render_template('profile_view.html', user=user)
-
-
-@auth.route('/users/<int:user_id>/edit', methods=['POST', 'GET'])
-@login_required
-def edit_user(user_id):
-    user = User.query.get(user_id)
-    form = user_edit_form(obj=user)
-    if form.validate_on_submit():
-        user.about = form.about.data
-        user.is_admin = int(form.is_admin.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('User Edited Successfully', 'success')
-        return redirect(url_for('auth.browse_users'))
-    return render_template('user_edit.html', form=form)
-
-
-@auth.route('/users/new', methods=['POST', 'GET'])
-@login_required
-def add_user():
-    form = register_form()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
-            db.session.add(user)
-            db.session.commit()
-            flash('Congratulations, you just created a user', 'success')
-            return redirect(url_for('auth.browse_users'))
-        else:
-            flash('Already Registered')
-            return redirect(url_for('auth.browse_users'))
-    return render_template('user_new.html', form=form)
-
-
-@auth.route('/users/<int:user_id>/delete', methods=['POST'])
-@login_required
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if user.id == current_user.id:
-        flash("You can't delete yourself!")
-        return redirect(url_for('auth.browse_users'), 302)
-    db.session.delete(user)
-    db.session.commit()
-    flash('User Deleted', 'success')
-    return redirect(url_for('auth.browse_users'), 302)
 
 
 @auth.route('/profile', methods=['POST', 'GET'])
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Project Setup
 
-[![Production Workflow](https://github.com/kaw393939/docker_flask/actions/workflows/prod.yml/badge.svg)](https://github.com/kaw393939/docker_flask/actions/workflows/prod.yml)
+[![Production Workflow](https://github.com/ttd22/docker_flask/actions/workflows/prod.yml/badge.svg)](https://github.com/kaw393939/docker_flask/actions/workflows/prod.yml)
 
 * [Production Deployment](https://kwilliam-prod.herokuapp.com/)
 
